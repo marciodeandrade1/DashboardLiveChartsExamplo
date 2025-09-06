b723c843248c5c93e637a222c142ad7915f27796
gmand <gmand@DESKTOP-2KO8PEB>
2025-09-06 09:26:44 -0400

    Criação das Models

---
+++ DashboardLiveChartsExamplo/Models/ParticipacaoMercado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashboardLiveChartsExamplo.Models
+{
+    public class ParticipacaoMercado
+    {
+        public string Empresa { get; set; }
+        public decimal Percentual { get; set; }
+    }
+}
